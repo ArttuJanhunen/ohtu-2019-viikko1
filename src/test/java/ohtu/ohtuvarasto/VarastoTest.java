48ab2a794ae041747e86a058f0c8ffec7fc8c84b
Arttu Janhunen <arttu.janhunen@gmail.com>
2019-10-31 16:20:34 +0200

    Testikattavuus nostettu 100%

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -2,6 +2,7 @@ package ohtu.ohtuvarasto;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import static org.junit.Assume.assumeNoException;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -63,6 +64,79 @@ public class VarastoTest {
 
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void voidaanLaittaaVainMaksimiTilavuus() {
+
+        varasto.lisaaVarastoon(100);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void liianPieniTilavuusKorjataanNollaan() {
+        Varasto virheellinen = new Varasto(0);
+
+        assertEquals(0.0, virheellinen.getTilavuus(), vertailuTarkkuus);
+
+        Varasto negatiivinenVarasto = new Varasto(-0.1);
+
+        assertEquals(0.0, negatiivinenVarasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void varastonToStringToimii() {
+
+        assertEquals("saldo = 0.0, vielä tilaa 10.0", varasto.toString());
+    }
+
+    @Test
+    public void otetaanNegatiivinenMaara() {
+
+        assertEquals(0.0, varasto.otaVarastosta(-5.0), vertailuTarkkuus);
+    }
+
+    @Test
+    public void ottaessaEnemmanKuinOnAnnetaanKaikki() {
+        varasto.lisaaVarastoon(5);
+
+        assertEquals(5, varasto.otaVarastosta(6), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisataanNegatiivinenMaara() {
+        varasto.lisaaVarastoon(5);
+        varasto.lisaaVarastoon(-2);
+
+        assertEquals(5.0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisataanEnemmanKuinMahtuu() {
+        varasto.lisaaVarastoon(200);
+
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void varastoaEiLuodaNegatiivisena() {
+        Varasto virheellinen = new Varasto(-5);
+
+        assertEquals(0.0, virheellinen.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void varastonSaldoEivoiOllaNegatiivinenLuodessa() {
+        Varasto virheellinen = new Varasto(10, -5);
+
+        assertEquals(0.0, virheellinen.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void varastoaLuodessaTilaaEiYlikuormiteta() {
+        Varasto virheellinen = new Varasto(10, 20);
+
+        assertEquals(10, virheellinen.getSaldo(), vertailuTarkkuus);
     }
 
 }
